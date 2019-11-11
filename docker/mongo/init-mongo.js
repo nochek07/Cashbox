3249c0b2948ecb3bcf4b27958b2c0aa1cbd4286f
nochek <nochek07@list.ru>
2019-11-11 15:06:37 +0300

    Docker-compose

---
+++ docker/mongo/init-mongo.js
@@ -0,0 +1,12 @@
+db.createUser(
+    {
+        user: "user",
+        pwd: "test",
+        roles: [
+            {
+                role: "readWrite",
+                db: "test"
+            }
+        ]
+    }
+)
